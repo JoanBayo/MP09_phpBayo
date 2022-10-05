2bb53561a7afbd4326c07f0f66736ccc2bd54d1d
Bayo <jbayo@iesebre.com>
2022-10-05 19:29:56 +0200

    Dia 1 final

--- php_for_laravel_developers/index.php
+++ php_for_laravel_developers/index.php
@@ -1,3 +1,19 @@
-<?php
 
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<!--Separation of concers -> Seprarar "coses diferents"/concers en diferents fitxers o seccions -> components-->
+<!--MVC -> MOdel Vista Controlador -> FC-->-->
+<?php
 echo '<h1>Hola mon!</h1>';
+?>
+<!--Minut video: https://youtu.be/s1h0SIt0g3I?list=PLyasg1A0hpk2vR_ocOHavVJiybYoB05pR&t=2025-->
+</body>
+</html>
